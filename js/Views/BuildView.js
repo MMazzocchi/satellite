211dd7a1f6edebb416bdefd9568bce066db98798
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-08 19:29:33 -0600

    Made a KICKASS build menu that AUTOSCROLLS on small screens WHO'S THE MASTER

--- js/Views/BuildView.js
+++ js/Views/BuildView.js
@@ -8,3 +8,12 @@ var BuildView = function() {
 
 BuildView.prototype = Object.create(View.prototype);
 BuildView.prototype.constructor = BuildView;
+
+BuildView.prototype.setupMenu = function() {
+    // Adjust the height of the build menu, forcing it to scroll
+    var buildMenu = $('#buildMenu')[0];
+    var navBar = $('#navHeader')[0];
+    var height = window.innerHeight - navBar.offsetHeight;
+
+    buildMenu.style.height = height + "px";
+};
