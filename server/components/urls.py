cc3fad993c8f2238e83eb76750e0490400869ceb
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-12 06:14:09 -0600

    Added a basic API to get components; I'd like to make it more general, but I think it's a decent solution for now.

--- server/components/urls.py
+++ server/components/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<component_name>[a-z_]+)/(?P<instance_id>[0-9]+)/$',
+        views.instance_data, name='instance_index'),
 ]
--- server/components/views.py
+++ server/components/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Chassis
 
 def index(request):
     return HttpResponse("Hello, welcome to the index.")
+
+def instance_data(request, component_name, instance_id):
+    response_template = loader.get_template('components/instance_data.json')
+
+    context = {
+        'component_name': component_name,
+        'instance_id': instance_id,
+        'instance_data': None
+    }
+
+    if component_name == "chassis":
+        data = None
+   
+        # Try to get the chassis with the given id 
+        try:
+            chassis = Chassis.objects.get(pk=instance_id)
+
+            # Render the specific chassis data, which will be passed into the
+            # components template.
+            chassis_template = loader.get_template('components/chassis.json')
+            chassis_ctx = {
+                'name': chassis.name,
+                'color': chassis.color,
+                'width': chassis.width,
+                'length': chassis.length,
+                'height': chassis.height
+            }
+
+            data = chassis_template.render(chassis_ctx, request)
+        except Chassis.DoesNotExist:
+            data = None
+    
+        context['instance_data'] = data
+
+    return render(request, 'components/instance_data.json', context)
