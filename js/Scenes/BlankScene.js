7a355c525ef4f09e3071646f9ddc07937c8816cc
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-13 02:17:47 -0600

    Removed the old website files.

--- js/Scenes/BlankScene.js
+++
@@ -1,8 +0,0 @@
-var BlankScene = function(satellite) {
-    Scene.call(this, "Blank Scene");
-};
-
-BlankScene.prototype = Object.create(Scene.prototype);
-BlankScene.prototype.constructor = BuildScene;
-
-BlankScene.prototype.step = function() {};
