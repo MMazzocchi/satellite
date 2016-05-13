2bf109f4e28e4b8b410b6e2ae08ea450e23f4a96
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-13 01:54:29 -0600

    Added an app to cover the current site; this will be seperate from the components API app.

--- server/satellite/urls.py
+++ server/satellite/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    # Component lookup API
     url(r'^components/', include('components.urls')),
+
+    # Admin site
     url(r'^admin/', admin.site.urls),
+
+    # Fallthrough to the regular site
+    url(r'^', include('website.urls'))
 ]
