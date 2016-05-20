286723b25a112b954809d507183569a419bfaebb
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-20 13:47:03 -0600

    Added a placeholder login page. That's right; get read for user data.

--- server/website/urls.py
+++ server/website/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 from .views import IndexView, BuildView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^js/BuildView.js$', BuildView.as_view(), name='build_view')
+    url(r'^js/BuildView.js$', BuildView.as_view(), name='build_view'),
+    url(r'^login/$', TemplateView.as_view(template_name='login.html'))
 ]
