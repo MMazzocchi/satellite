98531c6d47808b9471398730c69d7e210105ed4c
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-22 21:07:38 -0600

    Added a basic Satellite model.

--- server/website/models.py
+++ server/website/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from components.models import Chassis, CommDish, Batteries, SolarPanels
+from components.models import Storage, Sensors, Processor, FuelTank
+from components.models import Thrusters
+
 
 class SiteUser(models.Model):
     user = models.OneToOneField(User)
+    # TODO: Add money, satellites, jobs, basically everything.
 
     def __str__(self):
         return self.user.get_username()
+
+class Satellite(models.Model):
+    name        = models.CharField(max_length=50)
+
+    chassis     = models.ForeignKey(Chassis,     on_delete=models.PROTECT)
+    commDish    = models.ForeignKey(CommDish,    on_delete=models.PROTECT)
+    batteries   = models.ForeignKey(Batteries,   on_delete=models.PROTECT)
+    solarPanels = models.ForeignKey(SolarPanels, on_delete=models.PROTECT)
+    storage     = models.ForeignKey(Storage,     on_delete=models.PROTECT)
+    sensors     = models.ForeignKey(Sensors,     on_delete=models.PROTECT)
+    processor   = models.ForeignKey(Processor,   on_delete=models.PROTECT)
+    fuelTank    = models.ForeignKey(FuelTank,    on_delete=models.PROTECT)
+    thrusters   = models.ForeignKey(Thrusters,   on_delete=models.PROTECT)
