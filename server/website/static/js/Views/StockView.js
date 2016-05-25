5a4a7833a70b69cb59f946e002f32b70a4681665
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-24 20:49:11 -0600

    More skeleton code.

--- server/website/static/js/Views/StockView.js
+++ server/website/static/js/Views/StockView.js
@@ -5,11 +5,22 @@ var StockView = function() {
 
     this.menuId = "stockMenu";
     this.statusId = "stockStatus";
+
+    this.{{ type.name }}Options = {
+        index: 0,
+        total: {{ type.total }}
+    };
 };
 
 StockView.prototype = Object.create(View.prototype);
 StockView.prototype.constructor = StockView;
 
 StockView.prototype.setupMenu = function() {
+    $('#satellite-left').click(function() {
+
+    });
+    $('#satellite-right').click(function() {
+
+    });
 
 };
