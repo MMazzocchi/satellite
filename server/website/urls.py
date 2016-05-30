6203b078002a120bdb9710611f08b1395330c615
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-05-29 21:11:27 -0600

    Wired up purchase url to purchase view.

--- server/website/urls.py
+++ server/website/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^js/BuildView.js$', BuildView.as_view(), name='build_view'),
     url(r'^js/StockView.js$', views.stock_view, name='stock_view'),
     url(r'^satellite/(?P<num>[0-9]+)/?$', views.satellite_view, name='satellite_view'),
+    url(r'^purchase/?$', views.purchase_view, name='purchase_view'),
 ]
