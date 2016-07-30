8b9daf071faae17968b018c996f34cffe439bed1
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-07-29 23:10:04 -0600

    Reworked the jobs menu to use the template system. I'm never going back.

--- server/website/templates/js/JobsView.js
+++ server/website/templates/js/JobsView.js
@@ -21,39 +21,14 @@ var JobsView = function() {
 JobsView.prototype = Object.create(View.prototype);
 JobsView.prototype.constructor = JobsView;
 
-JobsView.prototype.getJobElement = function(job) {
-    var html = "<button type=\"button\" class=\"jobs-btn btn btn-default\"\">";
-    html += "<img class=\"icon img-thumbnail\" src=\"";
-
-    if(job.type == 1) {
-        html += "{% static 'images/commercial.png' %}\"/>Commercial";
-    } else if(job.type == 2) {
-        html += "{% static 'images/criminal.png' %}\"/>Criminal";
-    } else if(job.type == 3) {
-        html += "{% static 'images/government.png' %}\"/>Government";
-    } else if(job.type == 4) {
-        html += "{% static 'images/non_profit.png' %}\"/>Non-profit";
-    } else if(job.type == 5) {
-        html += "{% static 'images/science.png' %}\"/>Science";
-    }
-
-    html += " job #"+job.id+"</button>\n";
-
-    return html;
-};
-
 JobsView.prototype.setupMenu = function() {
     var statusPane = $('#statusPane')[0];
     if(statusPane.scroll) statusPane.scroll(0,0);
     var jobsMenu = $('#jobsMenu')[0];
     var jobsStatus = $('#jobsStatus')[0];
 
-    var html = "";
-    var total = 0;
-    for(var i in this.jobs) {
-        var job = this.jobs[i];
-        html += this.getJobElement(job);
-    }
-
-    jobsMenu.innerHTML = html;
+    // Fill the jobs menu with items created from the template
+    $("#jobsMenu").loadTemplate(
+      "{% static 'jquery_templates/job_menu_item_template.html' %}",
+      this.jobs);
 };
